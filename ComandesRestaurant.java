71402627897674340abbfaea4b32f4d3710b2f5f
Edgar <alu.edgar.rodrigo@mataro.epiaedu.cat>
2025-11-06 18:43:09 +0100

    first commit

---
+++ ComandesRestaurant.java
@@ -0,0 +1,12 @@
+public class ComandesRestaurant {
+    public static void main(String[] args) {
+        ComandesRestaurant p = new ComandesRestaurant();
+        p.principal();
+    }
+    public void principal() {
+        
+    }
+    public void mainMenu(){
+        
+    }
+}
